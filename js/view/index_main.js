067ba326319182481c21fc79fdbe951ab718b7f6
HuQian11 <hq_1314@163.com>
2017-04-01 16:16:40 +0800

    changeCode
    修改

--- js/view/index_main.js
+++ js/view/index_main.js
@@ -16,6 +16,7 @@ require.config({
 });
 require(["base", "bootstrap"], function (base, bootstrap) {
     function Index() {
+        this.maxnum=3;
         this.init();
     }
 
@@ -36,7 +37,29 @@ require(["base", "bootstrap"], function (base, bootstrap) {
          * time: 2013-05-17 22:58:32
          */
         render: function () {
+           this.bindEnvent.upEnvent.call(this);
+        },
+        bindEnvent:{
+            upEnvent:function(){
+                var that=this;
 
+                $('body').scrollspy({ target: '#navbar-example' });
+                $('[data-spy="scroll"]').each(function () {
+                    var $spy = $(this).scrollspy('refresh')
+                })
+                $('#myScrollspy').on('activate.bs.scrollspy', function () {
+                    var $spy = $(this).scrollspy('refresh')
+                })
+                $(document).on('click','div[name=up]',function () {
+                    var num=$(this).attr("data-num");
+                  if(that.maxnum<Number(num)){
+                      num=1;
+                  }
+                  $("#home"+num).removeClass('hide').siblings('div').addClass('hide');
+                  num++;
+                  $(this).attr("data-num",num);
+                });
+            }
         }
     };
 
